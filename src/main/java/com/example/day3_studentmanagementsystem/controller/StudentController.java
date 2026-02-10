277aae5047273720dd5f1a2afb661d7a0d1357e6
Anantika09 <anantikaag06@gmail.com>
2026-02-10 15:43:29 +0530

    Update StudentController.java

--- src/main/java/com/example/day3_studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/day3_studentmanagementsystem/controller/StudentController.java
@@ -4,28 +4,43 @@ import com.example.day3_studentmanagementsystem.dto.StudentRequestDto;
 import com.example.day3_studentmanagementsystem.dto.StudentResponseDto;
 import com.example.day3_studentmanagementsystem.model.StudentModel;
 import com.example.day3_studentmanagementsystem.service.StudentService;
+import com.example.day3_studentmanagementsystem.util.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
     private final StudentService service;
-
-    public StudentController(StudentService service) {
+    private final JwtUtil jwtUtil;
+    public StudentController(StudentService service, JwtUtil jwtUtil) {
         this.service = service;
+        this.jwtUtil = jwtUtil;
+    }
+    private void checkToken(String authHeader){
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            throw new RuntimeException("Invalid Token");
+        }
+        String token = authHeader.substring(7);
+        jwtUtil.validateTokenAndGetEmail(token);
+
     }
 
     // create function api
     @PostMapping("/add-student")
-    public StudentResponseDto addStudent(@Valid @RequestBody StudentRequestDto student){
+    public StudentResponseDto addStudent(
+            @RequestHeader("Authorization") String authHeader,
+            @Valid @RequestBody StudentRequestDto student){
+        checkToken(authHeader);
         return service.addStudent(student);
     }
 
     // display students
     @GetMapping("/students")
-    public List<StudentResponseDto> getStudents(){
+    public List<StudentResponseDto> getStudents(
+            @RequestHeader(value = "Authorization",required = false)String authHeader){
+        checkToken(authHeader);
         return service.getStudents();
     }
 
